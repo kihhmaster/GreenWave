95de86902f72f96663e939788b0581dc9f393e53
kihhmaster <kihhmaster@yandex.ru>
2022-04-28 18:54:44 +0300

    pop delete

--- app/js/app.js
+++ app/js/app.js
@@ -74,56 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	
 	
 
-	///popUP
-	let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
-	let popupBgTwo = document.querySelector('.popup__bg__two'); // Фон попап окна
-	let popup = document.querySelector('.popup'); // Само окно
-	let popupTwo = document.querySelector('.popupTwo'); // Само окн
-	let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
-	let openPopupButtonsTwo = document.querySelectorAll('.open-popup__two'); // Кнопки для показа окна
-	let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
-	let closePopupButtonTwo = document.querySelector('.close-popup__two'); // Кнопка для скрытия окна
-
-	openPopupButtons.forEach((button) => { // Перебираем все кнопки
-    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
-        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
-        popup.classList.add('active'); // И для самого окна
-    })
-	});
-	openPopupButtonsTwo.forEach((button) => { // Перебираем все кнопки
-    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
-        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-        popupBgTwo.classList.add('active'); // Добавляем класс 'active' для фона
-        popupTwo.classList.add('active'); // И для самого окна
-    })
-	});
-	closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
-    popupBg.classList.remove('active'); // Убираем активный класс с фона
-    popup.classList.remove('active'); // И с окна
-	});
-	closePopupButtonTwo.addEventListener('click',() => { // Вешаем обработчик на крестик
-    popupBgTwo.classList.remove('active'); // Убираем активный класс с фона
-    popupTwo.classList.remove('active'); // И с окна
-	});
-	document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
-    if(e.target === popupBg) { // Если цель клика - фот, то:
-        popupBg.classList.remove('active'); // Убираем активный класс с фона
-        popup.classList.remove('active'); // И с окна
-    }
-	});
-	document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
-    if(e.target === popupBgTwo) { // Если цель клика - фот, то:
-			popupBgTwo.classList.remove('active'); // Убираем активный класс с фона
-			popupTwo.classList.remove('active'); // И с окна
-    }
-	});
-	// document.body.onclick = () => {
-	// 	// Найти все открытые <details>
-	// 	document.body.querySelectorAll('.first__level[open]')
-	// 		// И закрыть каждый из них
-	// 		.forEach(e => e.open = false)
-	// }
+	
 
 	///popUP--finifh
 })
